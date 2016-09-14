8fbc46964eae72defda56f1be52e1eabcdab2657
Melanie27 <melaniemcganney@gmail.com>
2016-09-14 14:22:21 -0700

    initial commit

---
+++ com.melanie27.blocspot/com.melanie27.blocspot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  com.melanie27.blocspot
+//
+//  Created by MELANIE MCGANNEY on 9/14/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ com.melanie27.blocspot/com.melanie27.blocspotTests/com_melanie27_blocspotTests.m
@@ -0,0 +1,39 @@
+//
+//  com_melanie27_blocspotTests.m
+//  com.melanie27.blocspotTests
+//
+//  Created by MELANIE MCGANNEY on 9/14/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface com_melanie27_blocspotTests : XCTestCase
+
+@end
+
+@implementation com_melanie27_blocspotTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
